a699c9af4563ef6d264a9089a55d53c05f33f8a6
Brandon Satrom <bsatrom@gmail.com>
2019-02-01 10:12:47 -0600

    added swarm mode functionality on leader; closes #11

--- rc-swarm-leader/src/rc-swarm-leader.ino
+++ rc-swarm-leader/src/rc-swarm-leader.ino
@@ -17,20 +17,24 @@
 */
 
 // Wheel pin mappings
-int leftReverse = A0;
-int leftForward = A1;
-int rightForward = A2;
-int rightReverse = A3;
+int32_t leftReverse = A0;
+int32_t leftForward = A1;
+int32_t rightForward = A2;
+int32_t rightReverse = A3;
 
 int32_t lastLeftRVal = 0;
 int32_t lastLeftFVal = 0;
 int32_t lastRightRVal = 0;
 int32_t lastRightFVal = 0;
 
+const int32_t RC_MODE = 1;
+const int32_t DEMO_MODE = 2;
+
 #define MIN_PIN_VAL 150
 #define DRIVE_VAL 200
 
-String version = "v1.0";
+String version = "v1.1";
+int32_t mode = RC_MODE;
 
 void setup()
 {
@@ -42,21 +46,54 @@ void setup()
   pinMode(rightReverse, INPUT);
 
   Particle.publish("swarm-leader-online", version);
+  Particle.publish("swarm-mode", mode == 1 ? "RC Mode" : "Demo Mode");
   Particle.variable("fw-version", version);
+  Particle.variable("swarm-mode", mode);
+
+  Particle.function("switchMode", switchSwarmMode);
 }
 
 void loop()
 {
-  checkPin(leftReverse, &lastLeftRVal, "leftR");
-  checkPin(leftForward, &lastLeftFVal, "leftF");
-  checkPin(rightReverse, &lastRightRVal, "rightR");
-  checkPin(rightForward, &lastRightFVal, "rightF");
+  if (mode == RC_MODE)
+  {
+    checkPin(leftReverse, &lastLeftRVal, "leftR");
+    checkPin(leftForward, &lastLeftFVal, "leftF");
+    checkPin(rightReverse, &lastRightRVal, "rightR");
+    checkPin(rightForward, &lastRightFVal, "rightF");
+  }
+}
+
+int switchSwarmMode(String args)
+{
+  int32_t newMode = args.toInt();
+
+  if (newMode == 0) // It's a String
+  {
+    if (args == "rc")
+    {
+      mode = RC_MODE;
+    }
+    else if (args == "demo")
+    {
+      mode = DEMO_MODE;
+    }
+
+    Particle.publish("swarm-mode", mode == 1 ? "RC Mode" : "Demo Mode");
+  }
+  else
+  {
+    mode = newMode;
+
+    Particle.publish("swarm-mode", mode == 1 ? "RC Mode" : "Demo Mode");
+  }
+  return 1;
 }
 
 void checkPin(int pin, int32_t *lastVal, const char *event)
 {
   int32_t pinVal = analogRead(pin) / 16;
-  
+
   if (pinVal > MIN_PIN_VAL)
     pinVal = DRIVE_VAL;
   else
@@ -68,9 +105,11 @@ void checkPin(int pin, int32_t *lastVal, const char *event)
 
     Mesh.publish(event, String(DRIVE_VAL));
     Serial.printlnf("%s val: %i", event, DRIVE_VAL);
-  } else if (pinVal == 0 && *lastVal != 0) {
+  }
+  else if (pinVal == 0 && *lastVal != 0)
+  {
     *lastVal = 0;
-    
+
     Mesh.publish(event, String(0));
     Serial.printlnf("%s val: %i", event, 0);
   }
